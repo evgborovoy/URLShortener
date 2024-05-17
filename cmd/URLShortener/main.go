9b8966fbefef070551ce545c995c13d119ac6985
Evgeniy <borovoyevgen@gmail.com>
2024-05-17 12:11:49 +0400

    create logger

--- cmd/URLShortener/main.go
+++ cmd/URLShortener/main.go
@@ -1,14 +1,33 @@
 package main
 
-import "URLShortener/internal/config"
+import (
+	"URLShortener/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+	envDev   = "dev"
+)
 
 func main() {
 	config := config.MustLoad()
-	_ = config
+	logger := setupLogger(config.Env)
+	logger.Info("start urlshortener", slog.String("env", config.Env))
 
-	// init logger: slog
-	// init storage: sqlite
-	// init router: chi, "chi render"
-	// run server
+}
 
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	}
+	return log
 }
